0555cfbfce7b28dee51577b876e7101f64d156db
mirage <mirage.kamran.pkt@gmail.com>
2020-06-03 22:32:46 -0700

    MVP met

--- database.js
+++ database.js
@@ -1,45 +1,49 @@
 let users = [
-	{ id: "1", name: "Jane Doe" },
-	{ id: "2", name: "John Doe" },
-	{ id: "3", name: "Jack Doe" },
-]
+  { id: "1", name: "Jane Doe", bio: "the US president" },
+  { id: "2", name: "John Doe", bio: "Jane's Husband" },
+  { id: "3", name: "Jack Doe", bio: "Jane's Lover" },
+];
 
 function getUsers() {
-	return users
+  return users;
 }
 
 function getUserById(id) {
-	return users.find(u => u.id === id)
+  return users.find((u) => u.id === id);
 }
 
-function createUser(data) {
-	const payload = {
-		id: String(users.length + 1),
-		...data,
-	}
+// function getUserById(bio) {
+//   return users.find((u) => u.bio === bio);
+// }
 
-	users.push(payload)
-	return payload
+function createUser(data) {
+  const payload = {
+    id: String(users.length + 1),
+    ...data,
+  };
+
+  users.push(payload);
+  return payload;
 }
 
 function updateUser(id, data) {
-	const index = users.findIndex(u => u.id === id)
-	users[index] = {
-		...users[index],
-		...data,
-	}
-	
-	return users[index]
+  const index = users.findIndex((u) => u.id === id);
+  users[index] = {
+    ...users[index],
+    ...data,
+  };
+
+  return users[index];
 }
 
 function deleteUser(id) {
-	users = users.filter(u => u.id != id)
+  users = users.filter((u) => u.id != id);
 }
 
 module.exports = {
-	getUsers,
-	getUserById,
-	createUser,
-	updateUser,
-	deleteUser,
-}
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+};
--- server.js
+++ server.js
@@ -3,8 +3,20 @@ const db = require("./database.js");
 
 const server = express();
 
+// This is installing some middleware to allow Express
+// to parse JSON request bodies. We'll go more into detail about this later.
+server.use(express.json());
+
 server.get("/api/users", (req, res) => {
   const users = db.getUsers();
+
+//   if (user) {
+//     res.json(user);
+//   } else {
+//     res.status(500).json({
+//       errorMessage: "The user with the specified ID does not exist.",
+//     });
+//   }
   res.json(users);
 });
 
@@ -15,17 +27,53 @@ server.get("/api/users/:id", (req, res) => {
     res.json(user);
   } else {
     res.status(404).json({
-      message: "User not found",
+      errorMessage: "The user with the specified ID does not exist.",
     });
   }
 });
 
 server.post("/api/users", (req, res) => {
+  // never trust data coming from the client,
+  // always validate it to some degree. make sure it's what you're expecting
+  if (!req.body.name || !req.body.bio) {
+    return res.status(400).json({
+      message: "Please provide name and bio for the user.",
+    });
+  } 
+
   const newUser = db.createUser({
-    name: "Mirage Kamran",
+    name: req.body.name,
   });
 
   res.status(201).json(newUser);
+});
+
+server.put("/api/users/:id", (req, res) => {
+  const user = db.getUserById(req.params.id);
+  if (user) {
+    const updatedUser = db.updateUser(user.id, {
+      name: req.body.name || user.name,
+      bio: req.body.bio || user.bio,
+    });
+    res.json(updatedUser);
+  } else {
+    res.status(404).json({
+      message: "The user with the specified ID does not exist.",
+    });
+  }
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const user = db.getUserById(req.params.id);
+
+  if (user) {
+    db.deleteUser(user.id);
+    res.status(204).end();
+  } else {
+    res.status(404).json({
+      message: "The user with the specified ID does not exist.",
+    });
+  }
 });
 
 server.listen(8000, () => {
